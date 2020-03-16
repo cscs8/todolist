0c8737d5d5ae5f187d2a71e299b17e0ef42f5cb1
cs8 <42040439+cscs8@users.noreply.github.com>
2020-03-16 19:57:04 +0900

    fix Failed to load class org.slf4j.impl.StaticLoggerBinder

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 dependencies {
     "implementation"(kotlin("stdlib-jdk8"))
     "implementation"("com.sparkjava:spark-core:2.9.1")
+    "implementation"("org.slf4j:slf4j-simple:1.7.30")
 }
 
 tasks {

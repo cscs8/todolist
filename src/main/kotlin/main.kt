ca1358fa74117b4130b457ed40cb7d5f81176d2b
cs8 <42040439+cscs8@users.noreply.github.com>
2020-03-19 18:26:21 +0900

    update main.kt chapter 11 list 11.2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,20 @@
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import spark.Spark.get
+import todolist.Task
 
 fun main(args: Array<String>) {
+    val objectMapper = ObjectMapper().registerKotlinModule()
+
+    get("/tasks") { req, res ->
+        val tasks = listOf(
+            Task(1, "クリーニングに出す", false),
+            Task(2, "住民票を取得する", true)
+        )
+        objectMapper.writeValueAsString(tasks)
+    }
     get("/hello") { request, response ->
-        val name : String? = request.queryParams("name")
+        val name: String? = request.queryParams("name")
         "Hello, ${name ?: "world!"}"
     }
 }

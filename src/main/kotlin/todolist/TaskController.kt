a543a79183df876dc2066f4f2052b5535f3e1252
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-03 14:27:28 +0900

    fix List 11.18

--- src/main/kotlin/todolist/TaskController.kt
+++ src/main/kotlin/todolist/TaskController.kt
@@ -17,8 +17,8 @@ class TaskController(
     }
 
     fun show(): Route = Route { req, res ->
-        val id: Long? = req.params("id").toLongOrNull()
-        id?.let { taskRepository::findById } ?: throw halt(404)
+        val id = req.params("id").toLongOrNull()
+        id?.let(taskRepository::findById) ?: throw halt(404)
     }
 
     fun create(): Route = Route { req, res ->

9430b266d414314ff31d4e5f1f638e4f4edb7d26
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-03 12:00:59 +0900

    List 11.19

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,18 +6,15 @@ import todolist.JsonTransformer
 import todolist.TaskController
 import todolist.TaskRepository
 
-fun main(args: Array<String>) {
+fun main() {
     val objectMapper = ObjectMapper().registerKotlinModule()
     val jsonTransformer = JsonTransformer(objectMapper)
     val taskRepository = TaskRepository()
     val taskController = TaskController(objectMapper, taskRepository)
 
-    get(
-        "/tasks", taskController.index(),
-        jsonTransformer
-    )
+    get("/tasks", taskController.index(), jsonTransformer)
 
-    post(
-        "/tasks", taskController.create(), jsonTransformer
-    )
+    get("/tasks/:id", taskController.show(), jsonTransformer)
+
+    post("/tasks", taskController.create(), jsonTransformer)
 }

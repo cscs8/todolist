afe183315eac6bbaf51cb66e6bc8157b48f067b4
cs8080 <cs8.cs8080.cscs8@gmail.com>
2020-05-03 11:05:29 +0900

    List 11.16

--- src/main/kotlin/todolist/TaskController.kt
+++ src/main/kotlin/todolist/TaskController.kt
@@ -8,17 +8,17 @@ import spark.Spark
 import spark.Spark.halt
 import java.lang.Exception
 
-class TaskController(private val objectMapper: ObjectMapper, private val taskRepository: TaskRepository) {
-    fun index(): Route = Route { req, res ->
+class TaskController(
+    private val objectMapper: ObjectMapper,
+    private val taskRepository: TaskRepository
+) {
+    fun index(): Route = Route { _, _ ->
         taskRepository.findAll()
     }
 
     fun create(): Route = Route { req, res ->
-        val request: TaskCreateRequest = try {
-            objectMapper.readValue(req.bodyAsBytes(), TaskCreateRequest::class.java)
-        } catch (e: Exception) {
-            throw halt(400)
-        }
+        val request: TaskCreateRequest =
+            objectMapper.readValue(req.bodyAsBytes()) ?: throw halt(400)
         val task = taskRepository.create(request.content)
         res.status(201)
         task
